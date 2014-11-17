f7f8caed955ee05584b4e89428b49f132004540c
arvidsson <arvidsson@gmail.com>
2014-11-17 22:05:01 +0100

    Added add and get component

--- README.md
+++ README.md
@@ -6,36 +6,42 @@ A minimal entity-component system
 example
 -------
 
-    struct PositionComponent : Component<PositionComponent>
-    {
+    struct PositionComponent : Component<PositionComponent> {
         PositionComponent(int x = 0, int y = 0) : x(x), y(y) {}
         int x, y;
     };
 
-    struct VelocityComponent : Component<VelocityComponent>
-    {
+    struct VelocityComponent : Component<VelocityComponent> {
         VelocityComponent(int x = 0, int y = 0) : x(x), y(y) {}
         int x, y;
     };
 
-    class MovementSystem : public System<MovementSystem>
-    {
+    class MovementSystem : public System<MovementSystem> {
     public:
-        MovementSystem()
-        {
-            AddComponentType<PositionComponent>();
-            AddComponentType<VelocityComponent>();
+        MovementSystem() {
+            register_component<PositionComponent>();
+            register_component<VelocityComponent>();
         }
 
-        virtual void Update(float delta)
-        {
-            for (auto e : entities)
-            {
-                PositionComponent &pos = e.GetComponent<PositionComponent>();
-                const VelocityComponent &vel = e.GetComponent<VelocityComponent>();
+        virtual void update(float delta) {
+            for (auto e : entities) {
+                PositionComponent &pos = e.get_component<PositionComponent>();
+                const VelocityComponent &vel = e.get_component<VelocityComponent>();
 
                 pos.x += vel.x * delta;
                 pos.y += vel.y * delta;
             }
         }
     };
+
+    int main() {
+        World world;
+
+        SystemManager &system_manager = world.get_system_manager();
+        system_manager.add_system<MovementSystem>();
+
+        Entity e = world.create_entity();
+        e.add_component<PositionComponent>(100, 50);
+        e.add_component<VelocityComponent>(100, 0);
+        e.finalize();
+    }
--- src/entity.hpp
+++ src/entity.hpp
@@ -19,6 +19,12 @@ namespace entity {
 
         Id get_id() const { return id; }
 
+        template <typename T, typename ... Args>
+        void add_component(Args && ... args);
+
+        template <typename T>
+        T& get_component();
+
     private:
         Id id;
         EntityManager &entity_manager;
@@ -28,6 +34,12 @@ namespace entity {
     public:
         Entity::Id create_entity();
         void remove_entity(Entity::Id id);
+
+        template <typename T, typename ... Args>
+        void add_component(Entity::Id id, Args && ... args);
+
+        template <typename T>
+        T& GetComponent(Entity::Id id);
 
     private:
         static Entity::Id id_counter;
@@ -41,6 +53,49 @@ namespace entity {
         // available entity ids
         std::vector<Entity::Id> reusable_ids;
     };
+
+    template <typename T, typename ... Args>
+    void Entity::add_component(Args && ... args) {
+        entity_manager.add_component<T>(id, std::forward<Args>(args)...);
+    }
+
+    template <typename T>
+    T& Entity::get_component() {
+        return entity_manager.get_component<T>(id);
+    }
+
+    template <typename T, typename ... Args>
+    void EntityManager::add_component(Entity::Id id, Args && ... args) {
+        const BaseComponent::Type type = T::get_type();
+        std::shared_ptr<Pool<T>> pool;
+
+        if (component_pools.size() <= type) {
+            component_pools.resize(type + 1);
+        }
+
+        if (!component_pools[type]) {
+            pool = std::shared_ptr<Pool<T>>(new Pool<T>());
+            component_pools[type] = pool;
+        }
+        else {
+            pool = std::static_pointer_cast<Pool<T>>(component_pools[type]);
+        }
+
+        pool->reserve(id_counter);
+        T component(std::forward<Args>(args) ...);
+        pool->set(id, component);
+
+        components_per_entity[id].set(type);
+    }
+
+    template <typename T>
+    T& EntityManager::get_component(Entity::Id id) {
+        const BaseComponent::Type type = T::get_type();
+        assert(type < component_pools.size());
+        std::shared_ptr<Pool<T>> pool = std::static_pointer_cast<Pool<T>>(component_pools[type]);
+        assert(pool);
+        return pool->get(id);
+    }
 
 }
 
