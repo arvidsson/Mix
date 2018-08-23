d639fdd58cd21f98edc7d6ad8e99df856889fdc3
arvidsson <arvidsson@gmail.com>
2018-08-23 08:08:00 +0200

    Cleaned up the world

---
+++ entity/World.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Entity.h"
+#include "System.h"
+#include "Event.h"
+#include <vector>
+#include <string>
+#include <memory>
+
+namespace entity
+{
+
+// The World manages the creation and destruction of entities so that entities.
+class World
+{
+public:
+    World();
+
+    EntityManager& getEntityManager() const;
+    SystemManager& getSystemManager() const;
+    EventManager& getEventManager() const;
+
+    /*
+        Updates the systems so that created/deleted entities are removed from the systems' vectors of entities.
+        Updates the entity manager so that the version of a destructed entity's index is incremented.
+        Destroys all the events that were created during the last frame.
+    */
+    void update();
+
+    Entity createEntity();
+    void destroyEntity(Entity e);
+
+    Entity getEntity(std::string tag) const;
+    std::vector<Entity> getGroup(std::string group) const;
+
+private:
+    // vector of entities that are awaiting creation
+    std::vector<Entity> createdEntities;
+
+    // vector of entities that are awaiting destruction
+    std::vector<Entity> destroyedEntities;
+
+    std::unique_ptr<EntityManager> entityManager = nullptr;
+    std::unique_ptr<SystemManager> systemManager = nullptr;
+    std::unique_ptr<EventManager> eventManager = nullptr;
+};
+
+}
