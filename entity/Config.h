ae420fd30e93a5c8d645d060efc9d4e196ca4272
arvidsson <arvidsson@gmail.com>
2018-08-23 08:03:05 +0200

    Added default pool size to config

--- entity/Config.h
+++ entity/Config.h
@@ -8,7 +8,8 @@ enum
     MAX_COMPONENTS   = 64,
     INDEX_BITS       = 24,
     VERSION_BITS     = 8,
-    MINIMUM_FREE_IDS = 1024
+    MINIMUM_FREE_IDS = 1024,
+    DEFAULT_POOL_SIZE = 100
 };
 
 }
